0a03cffd07e9743295a09fbede297f9f084d2ede
Arslan <nik.nikken.96@mail.ru>
2025-08-09 00:29:35 +0300

    crud api с docker и postgresql

--- go.mod
+++ go.mod
@@ -5,9 +5,16 @@ go 1.24.0
 require (
 	github.com/google/uuid v1.6.0
 	github.com/labstack/echo/v4 v4.13.4
+	gorm.io/driver/postgres v1.5.6
+	gorm.io/gorm v1.25.10
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -1,87 +1,98 @@
-package main
+﻿package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 type Task struct {
-	ID   string `json:"id"`
-	Text string `json:"task"`
+	ID     string `gorm:"primaryKey" json:"id"`
+	Text   string `json:"task"`
+	IsDone bool   `gorm:"default:false" json:"default:false"`
 }
 
-var tasks []Task
+func initDB() {
+	dsn := "host=localhost user=postgres password=12345 dbname=tasks port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database", err)
+	}
 
-func getTask(c echo.Context) error {
-	if tasks == nil {
-		return c.JSON(http.StatusOK, []Task{})
+	db.AutoMigrate(&Task{})
+}
+
+func getTasks(c echo.Context) error {
+	var tasks []Task
+	if err := db.Find(&tasks).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "database error"})
 	}
 	return c.JSON(http.StatusOK, tasks)
 }
 
-func postTask(c echo.Context) error {
-	var request struct {
-		Task string `json: "task"`
-	}
-	if err := c.Bind(&request); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
+func createTask(c echo.Context) error {
+	var task Task
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "invalid request"})
 	}
 
-	task := Task{
-		ID:   uuid.NewString(),
-		Text: request.Task,
+	task.ID = uuid.NewString()
+
+	if err := db.Create(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "failed to create task"})
 	}
-	tasks = append(tasks, task)
 
 	return c.JSON(http.StatusCreated, task)
 }
 
-func patchTask(c echo.Context) error {
+func updateTask(c echo.Context) error {
 	id := c.Param("id")
-	var request struct {
-		Task string `json: "task"`
+	var task Task
+	if err := db.First(&task, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
 	}
-	if err := c.Bind(&request); err != nil {
+
+	if err := c.Bind(&task); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
 	}
 
-	for i, task := range tasks {
-		if task.ID == id {
-			tasks[i].Text = request.Task
-			return c.JSON(http.StatusOK, tasks[i])
-		}
+	if err := db.Save(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "falied to update task"})
 	}
-	return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
+
+	return c.JSON(http.StatusOK, task)
 }
 
 func deleteTask(c echo.Context) error {
 	id := c.Param("id")
-
-	for i, task := range tasks {
-		if task.ID == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			return c.NoContent(http.StatusNoContent)
-		}
+	if err := db.Delete(&Task{}, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "failed to delete task"})
 	}
-	return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
+	initDB()
 	e := echo.New()
-
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
-	e.GET("/task", getTask)
-	e.POST("/task", postTask)
-	e.PATCH("/task/:id", patchTask)
-	e.DELETE("/task/:id", deleteTask)
+	e.GET("/tasks", createTask)
+	e.POST("/tasks", getTasks)
+	e.PATCH("/tasks/:id", updateTask)
+	e.DELETE("/tasks/:id", deleteTask)
 
-	fmt.Println("сервер запущен")
+	fmt.Println("http://localhost:8080")
+	e.Start(":8080")
 
-	e.Logger.Fatal(e.Start("localhost:8080"))
 }
