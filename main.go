9958a95c10a7e542dcc35ca6d1d625728c0fee75
Arslan <nik.nikken.96@mail.ru>
2025-08-20 02:14:03 +0300

    пофиксил ручки

--- main.go
+++ main.go
@@ -14,59 +14,81 @@ import (
 
 var db *gorm.DB
 
-type Task struct {
-	ID     string `gorm:"primaryKey" json:"id"`
-	Text   string `json:"task"`
-	IsDone bool   `gorm:"default:false" json:"default:false"`
-}
-
 func initDB() {
 	dsn := "host=localhost user=postgres password=12345 dbname=tasks port=5432 sslmode=disable"
 	var err error
+
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("failed to connect to database", err)
+		log.Fatalf("could not connect to database: %v", err)
 	}
 
-	db.AutoMigrate(&Task{})
+	if err := db.AutoMigrate(&Task{}); err != nil {
+		log.Fatalf("could not migrate: %v", err)
+	}
 }
 
-func getTasks(c echo.Context) error {
-	var tasks []Task
-	if err := db.Find(&tasks).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "database error"})
-	}
-	return c.JSON(http.StatusOK, tasks)
+type Task struct {
+	ID     string `gorm:"primaryKey" json:"id"`
+	Text   string `json:"task"`
+	IsDone bool   `json:"is_done"`
 }
 
-func createTask(c echo.Context) error {
-	var task Task
-	if err := c.Bind(&task); err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "invalid request"})
+func postTask(c echo.Context) error {
+	var req struct {
+		Text   string `json:"task"`
+		IsDone bool   `json:"is_done"`
 	}
 
-	task.ID = uuid.NewString()
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
+	}
+
+	task := Task{
+		ID:     uuid.NewString(),
+		Text:   req.Text,
+		IsDone: req.IsDone,
+	}
 
 	if err := db.Create(&task).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "failed to create task"})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not add task"})
 	}
 
 	return c.JSON(http.StatusCreated, task)
 }
 
-func updateTask(c echo.Context) error {
+func getTasks(c echo.Context) error {
+	var tasks []Task
+
+	if err := db.Find(&tasks).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not get tasks"})
+	}
+
+	return c.JSON(http.StatusOK, tasks)
+}
+
+func putTask(c echo.Context) error {
 	id := c.Param("id")
+
+	var req struct {
+		Text   string `json:"task"`
+		IsDone bool   `json:"is_done"`
+	}
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
 	var task Task
 	if err := db.First(&task, "id = ?", id).Error; err != nil {
-		return c.JSON(http.StatusNotFound, map[string]string{"error": "task not found"})
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "could not find task"})
 	}
 
-	if err := c.Bind(&task); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid request"})
-	}
+	task.Text = req.Text
+	task.IsDone = req.IsDone
 
 	if err := db.Save(&task).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "falied to update task"})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not update task"})
 	}
 
 	return c.JSON(http.StatusOK, task)
@@ -74,8 +96,9 @@ func updateTask(c echo.Context) error {
 
 func deleteTask(c echo.Context) error {
 	id := c.Param("id")
+
 	if err := db.Delete(&Task{}, "id = ?", id).Error; err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "failed to delete task"})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "could not delete task"})
 	}
 
 	return c.NoContent(http.StatusNoContent)
@@ -87,12 +110,11 @@ func main() {
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
-	e.GET("/tasks", createTask)
-	e.POST("/tasks", getTasks)
-	e.PATCH("/tasks/:id", updateTask)
+	e.GET("/tasks", getTasks)
+	e.POST("/tasks", postTask)
+	e.PATCH("/tasks/:id", putTask)
 	e.DELETE("/tasks/:id", deleteTask)
 
 	fmt.Println("http://localhost:8080")
 	e.Start(":8080")
-
 }
